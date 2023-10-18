063ac5c8d4360771810452faf2dc2ea64aa8116a
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-10-18 10:29:03 +0600

    added dynamic router title

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,6 +1,10 @@
+import { Helmet } from "react-helmet";
 const Home = () => {
   return (
     <div>
+      <Helmet>
+        <title>TechHub | Home</title>
+      </Helmet>
       <h2>home</h2>
     </div>
   );
